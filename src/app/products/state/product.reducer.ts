ba9fdc8ebe93b94d092d9d7dc4af8bd3033fdcba
Markfady <markfady9@gmail.com>
2024-05-20 12:28:49 +0300

    use actions inside our reducer

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,6 +1,6 @@
 import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
 import { Product } from "../product";import { state } from "@angular/animations";
-
+import * as ProductsActions from './product.action'
 export interface ProductState{
     showProductCode:boolean;
     currentProduct:Product;
@@ -17,11 +17,35 @@ export const getShowProductCode=createSelector(
     state=>state.showProductCode
 )
 export const productReducer=createReducer<ProductState>(initialState,
-    on(createAction('Toggle Product code'),(state):ProductState=>{
+    on(ProductsActions.toggleProductCode,(state):ProductState=>{
         console.log('original state'+JSON.stringify(state))
         return{
             ...state,
             showProductCode:!state.showProductCode,
                 }
+    }),
+    on(ProductsActions.setCurrentProduct,(state,action):ProductState=>{ //setCurrentProduct needs data to pass so we strongly typed it too here with using action
+        return{
+            ...state,
+            currentProduct:action.product
+        }
+    }),
+    on(ProductsActions.clearCurrentProduct,(state):ProductState=>{
+        return{
+            ...state,
+            currentProduct:null as any
+        }
+    }),
+    on(ProductsActions.initializeCurrentProduct,(state):ProductState=>{
+        return{
+            ...state,
+            currentProduct:{
+                id:0,
+                productName:'',
+                productCode:'New',
+                description:'',
+                starRating:0
+            }
+        }
     })
 )

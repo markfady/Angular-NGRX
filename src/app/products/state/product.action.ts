6c38b4963cf294f7359fcf1a60661c2c619e261e
Markfady <markfady9@gmail.com>
2024-05-20 13:44:03 +0300

    added new actions

--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -10,3 +10,17 @@ export const setCurrentProduct=createAction('setCurrentProduct', //passing data 
 export const clearCurrentProduct=createAction('Clear Current Product');
 
 export const initializeCurrentProduct=createAction('Initialize Current Product');
+ /* Complex operations*/
+export const loadProducts = createAction(
+    '[Product] Load'
+  );
+  
+  export const loadProductsSuccess = createAction(
+    '[Product] Load Success',
+    props<{ products: Product[] }>()
+  );
+  
+  export const loadProductsFailure = createAction(
+    '[Product] Load Fail',
+    props<{ error: string }>()
+  );

d3d361ebe3d394b66bb064836fd9cbb2e48d24b8
Markfady <markfady9@gmail.com>
2024-05-20 13:31:56 +0300

    subscribe to store , dispatch setCurrentProduct

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import { Subscription } from 'rxjs';
-
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { GenericValidator } from '../../shared/generic-validator';
 import { NumberValidators } from '../../shared/number.validator';
-
+import * as ProductsActions from '../state/product.action'
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/state/app.state';
+import { getCurrentProduct } from '../state/product.reducer';
 @Component({
   selector: 'pm-product-edit',
   templateUrl: './product-edit.component.html'
 })
-export class ProductEditComponent implements OnInit, OnDestroy {
+export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage = '';
   productForm: FormGroup;
   product: Product | null;
-  sub: Subscription;
+
 
   // Use with the generic validation message class
   displayMessage: { [key: string]: string } = {};
   private validationMessages: { [key: string]: { [key: string]: string } };
   private genericValidator: GenericValidator;
 
-  constructor(private fb: FormBuilder, private productService: ProductService) {
+  constructor( private store:Store<State>,private fb: FormBuilder, private productService: ProductService) {
 
     // Defines all of the validation messages for the form.
     // These could instead be retrieved from a file or database.
@@ -57,7 +57,7 @@ export class ProductEditComponent implements OnInit, OnDestroy {
     });
 
     // Watch for changes to the currently selected product
-    this.sub = this.productService.selectedProductChanges$.subscribe(
+    this.store.select(getCurrentProduct).subscribe(
       currentProduct => this.displayProduct(currentProduct)
     );
 
@@ -67,9 +67,6 @@ export class ProductEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   // Also validate on blur
   // Helpful if the user tabs through required fields
@@ -112,13 +109,13 @@ export class ProductEditComponent implements OnInit, OnDestroy {
     if (product && product.id) {
       if (confirm(`Really delete the product: ${product.productName}?`)) {
         this.productService.deleteProduct(product.id).subscribe({
-          next: () => this.productService.changeSelectedProduct(null),
+          next: () =>this.store.dispatch(ProductsActions.clearCurrentProduct()),
           error: err => this.errorMessage = err
         });
       }
     } else {
       // No need to delete, it was never saved
-      this.productService.changeSelectedProduct(null);
+      this.store.dispatch(ProductsActions.clearCurrentProduct())
     }
   }
 
@@ -132,12 +129,12 @@ export class ProductEditComponent implements OnInit, OnDestroy {
 
         if (product.id === 0) {
           this.productService.createProduct(product).subscribe({
-            next: p => this.productService.changeSelectedProduct(p),
+            next: p => this.store.dispatch(ProductsActions.setCurrentProduct({product:p})),
             error: err => this.errorMessage = err
           });
         } else {
           this.productService.updateProduct(product).subscribe({
-            next: p => this.productService.changeSelectedProduct(p),
+            next: p =>this.store.dispatch(ProductsActions.setCurrentProduct({product:p})),
             error: err => this.errorMessage = err
           });
         }

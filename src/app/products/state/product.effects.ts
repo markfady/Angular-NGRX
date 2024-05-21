a9cc5fc6f9d53370f37abcee49dd4acffc3a77f7
Markfady <markfady9@gmail.com>
2024-05-21 13:55:22 +0300

    update effect action with the services

--- src/app/products/state/product.effects.ts
+++ src/app/products/state/product.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ProductService } from "../product.service";
 import * as ProductsActions from "../state/product.action"
-import { map, mergeMap } from "rxjs";
+import { catchError, concatMap, map, mergeMap, of } from "rxjs";
 @Injectable()
 export class ProductEffects{
     constructor( private actions$:Actions, private productService:ProductService){}
@@ -16,5 +16,14 @@ export class ProductEffects{
                 ))
             )
         })
-    
+        updateProduct$=createEffect(()=>{ //takes callback function and return new action
+            //listen to action to filter on and pass operator using of method
+            return this.actions$.pipe( //respond to any dispatched action
+                ofType(ProductsActions.updateProduct), //if the dispatched action is updateProduct we call productService
+                concatMap((action)=>this.productService.updateProduct(action.product).pipe( //this make http put request to update product to back end server
+                    map(product=>ProductsActions.updateProductSuccess({product})), //after the success we pass the returned data to the updateProductSuccess
+                    catchError(error=>of(ProductsActions.updateProductFailure({error}))) // we use of cause catchError donesn't return observable so return dispateched action as observable using of 
+                )) 
+            )
+        })
 }
